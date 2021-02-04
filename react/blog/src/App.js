3e25552c4e3de54ca1424fd46d77279ed8ba5f6c
woo-ki <ziziza93@naver.com>
2021-02-04 21:49:51 +0900

    2021-02-04 21:49 react state 개념 학습

--- react/blog/src/App.js
+++ react/blog/src/App.js
@@ -1,16 +1,36 @@
+import React, {useState} from 'react'
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  let posts = '강남 고기 맛집';
-
+  let [글제목, 글제목변경] = useState(['남자 코트 추천', '강남 우동 맛집', '재미있게 노는법']);
+  let [따봉, 따봉변경] = useState(0);
+  function 제목바꾸기() {
+    let newArray = [...글제목].sort();
+    글제목변경(newArray);
+  }
+  
   return (
     <div className="App">
       <div className="black-nav">
-        <div style={{color: 'blue', fontSize: '30px'}}>개발 블로그</div>
+        <div>개발 블로그</div>
       </div>
-      <img src={logo} />
-      <h4>{posts}</h4>
+      <button onClick={제목바꾸기}>버튼</button>
+      <div className="list">
+        <h3>{글제목[0]} <input type="button" value="👍" onClick={()=>{따봉변경(따봉 + 1)}} /> {따봉}</h3>
+        <p>2월 17일 발행</p>
+        <hr/>
+      </div>
+      <div className="list">
+        <h3>{글제목[1]}</h3>
+        <p>2월 13일 발행</p>
+        <hr/>
+      </div>
+      <div className="list">
+        <h3>{글제목[2]}</h3>
+        <p>2월 2일 발행</p>
+        <hr/>
+      </div>
     </div>
   );
 }
