8b79598cedf75e6796fd5e5f2ac74e1ba7815b59
woo-ki <ziziza93@naver.com>
2021-02-05 21:41:13 +0900

    2021-02-05 21:41 react 컴포넌트 개념 학습

--- react/blog/src/App.js
+++ react/blog/src/App.js
@@ -1,23 +1,23 @@
 import React, {useState} from 'react'
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   let [글제목, 글제목변경] = useState(['남자 코트 추천', '강남 우동 맛집', '재미있게 노는법']);
   let [따봉, 따봉변경] = useState(0);
-  function 제목바꾸기() {
-    let newArray = [...글제목].sort();
-    글제목변경(newArray);
-  }
+  let [modal, modalUpdate] = useState(false);
+  // function 제목바꾸기() {
+  //   let newArray = [...글제목].sort();
+  //   글제목변경(newArray);
+  // }
   
   return (
     <div className="App">
       <div className="black-nav">
         <div>개발 블로그</div>
       </div>
-      <button onClick={제목바꾸기}>버튼</button>
+      {/* <button onClick={제목바꾸기}>버튼</button> */}
       <div className="list">
-        <h3>{글제목[0]} <input type="button" value="👍" onClick={()=>{따봉변경(따봉 + 1)}} /> {따봉}</h3>
+        <h3>{글제목[0]} <input style={{background: 'white', border: 'none'}} type="button" value="👍" onClick={()=>{따봉변경(따봉 + 1)}} /> {따봉}</h3>
         <p>2월 17일 발행</p>
         <hr/>
       </div>
@@ -31,8 +31,21 @@ function App() {
         <p>2월 2일 발행</p>
         <hr/>
       </div>
+
+      <button onClick={()=>{modalUpdate(!modal)}}>버튼</button>
+      {modal ? <Modal></Modal> : null}
     </div>
   );
+}
+
+function Modal() {
+  return (
+    <div className="modal">
+        <h2>제목</h2>
+        <p>날짜</p>
+        <p>상세내용</p>
+    </div>
+  )
 }
 
 export default App;
