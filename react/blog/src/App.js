a02b2ef67764826c143964f4694473c2cf97d21c
woo-ki <ziziza93@naver.com>
2021-02-06 21:05:36 +0900

    2021-02-06 21:05 react map을 이용한 반복문처리 + for 반복문 작성방법

--- react/blog/src/App.js
+++ react/blog/src/App.js
@@ -3,12 +3,32 @@ import './App.css';
 
 function App() {
   let [글제목, 글제목변경] = useState(['남자 코트 추천', '강남 우동 맛집', '재미있게 노는법']);
-  let [따봉, 따봉변경] = useState(0);
+  let [작성일, 작성일변경] = useState(['2월 14일 발행', '2월 15일 발행', '2월 17일 발행']);
+  let [따봉, 따봉변경] = useState([0, 0, 0]);
   let [modal, modalUpdate] = useState(false);
   // function 제목바꾸기() {
   //   let newArray = [...글제목].sort();
   //   글제목변경(newArray);
   // }
+  function 따봉바꾸기(idx) {
+    let new따봉 = [...따봉];
+    new따봉[idx] = new따봉[idx] + 1;
+    따봉변경(new따봉);
+  }
+
+  function 게시물작성() {
+    let 게시물 = [];
+    for(let i = 0; i < 글제목.length; i++) {
+      게시물.push(
+        <div className="list">
+          <h3>{글제목[i]} <input type="button" value="👍" onClick={()=>{따봉바꾸기(i)}} /> {따봉[i]}</h3>
+          <p>{작성일[글제목.length - i - 1]}</p>
+          <hr/>
+        </div>
+      )
+    }
+    return 게시물;
+  }
   
   return (
     <div className="App">
@@ -16,21 +36,18 @@ function App() {
         <div>개발 블로그</div>
       </div>
       {/* <button onClick={제목바꾸기}>버튼</button> */}
-      <div className="list">
-        <h3>{글제목[0]} <input style={{background: 'white', border: 'none'}} type="button" value="👍" onClick={()=>{따봉변경(따봉 + 1)}} /> {따봉}</h3>
-        <p>2월 17일 발행</p>
-        <hr/>
-      </div>
-      <div className="list">
-        <h3>{글제목[1]}</h3>
-        <p>2월 13일 발행</p>
-        <hr/>
-      </div>
-      <div className="list">
-        <h3>{글제목[2]}</h3>
-        <p>2월 2일 발행</p>
-        <hr/>
-      </div>
+      {/* {게시물작성()} */}
+      {
+        글제목.map((key, idx) => {
+          return (
+            <div className="list">
+              <h3>{key} <input type="button" value="👍" onClick={()=>{따봉바꾸기(idx)}} /> {따봉[idx]}</h3>
+              <p>{작성일[글제목.length - idx - 1]}</p>
+              <hr/>
+            </div>
+          )
+        })
+      }
 
       <button onClick={()=>{modalUpdate(!modal)}}>버튼</button>
       {modal ? <Modal></Modal> : null}
