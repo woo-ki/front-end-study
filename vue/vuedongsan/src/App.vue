c02038b008e5d6492583ab3c0629802a0325875a
woo-ki <ziziza93@naver.com>
2021-06-04 19:00:29 +0900

    Vue 세팅
    - 뷰 프로젝트 생성
    - v-for 학습

---
+++ vue/vuedongsan/src/App.vue
@@ -0,0 +1,56 @@
+<template>
+
+  <div class="menu">
+    <a v-for="(name, i) in menus" :key="i">{{name}}</a>
+  </div>
+
+  <div v-for="(info, i) in infos" :key="i">
+    <img src="./assets/room0.jpg">
+    <h4>{{info.product}}</h4>
+    <p>{{info.price}}</p>
+    <button v-on:click="increase(i)">허위매물신고</button> <span>신고수 : {{info.report}}</span>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      infos: [
+        {product: '역삼동원룸', price: '50만원', report: 0, filename: './assets/room0.jpg'},
+        {product: '천호동원룸', price: '60만원', report: 0, filename: './assets/room1.jpg'},
+        {product: '마포구원룸', price: '40만원', report: 0, filename: './assets/room2.jpg'}
+      ],
+      menus: ['Home', 'Shop', 'About']
+    }
+  },
+  methods: {
+    increase(i) {
+      this.info[i].report++
+    }
+  },
+  components: {
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+.menu {
+  background: darkslateblue;
+  padding: 15px;
+  border-radius: 5px;
+}
+.menu a {
+  color: white;
+  padding: 10px;
+}
+</style>
